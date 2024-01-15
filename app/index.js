f48713222f7538ee8097b32f41e80ef131e0fe21
KamelAkar <kemelakar0@gmail.com>
2024-01-15 14:26:34 +0100

    Push methode Pub and Sub

--- app/index.js
+++ app/index.js
@@ -4,53 +4,37 @@ const redis = require('redis');
 
 const app = express();
 const client = redis.createClient(6379);
+const subscriber = client.duplicate();
+
+// Middleware pour analyser les requêtes JSON
+app.use(express.json());
 
 (async () => {
-  client.on('error', (err) => {
-    console.log('Redis Client Error', err);
-  });
-  client.on('ready', () => console.log('Redis is ready'));
-
+  client.on('error', (err) => console.log('Redis Client Error', err));
   await client.connect();
-
-  await client.ping();
+  await subscriber.connect();
 })();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+
+// Route pour publier un message
+app.post('/publish/:channel', async (req, res) => {
+  const { channel } = req.params;
+  console.log(req.body);
+  const message = req.body.message;
+
+  await client.publish(channel, message);
+  res.send(`Message publié dans le canal ${channel}`);
 });
 
+// Route pour s'abonner à un canal
+app.get('/subscribe/:channel', (req, res) => {
+  const { channel } = req.params;
 
-app.get('/users', async (req, res) => {
-  const cacheKey = 'users';
-  const data = await client.get(cacheKey);
+  subscriber.subscribe(channel, (message) => {
+    console.log(`Reçu: ${message} sur le canal ${channel}`);
+  });
 
-  if (data != null) {
-    // Données en cache disponibles
-    res.send(JSON.parse(data));
-  } else {
-    // Données non disponibles dans le cache, effectuez la requête
-    const users = await axios.get('https://jsonplaceholder.typicode.com/users');
-    client.setEx(cacheKey, 3600, JSON.stringify(users.data)); // Mettez en cache pendant 1 heure
-    res.send(users.data);
-  }
+  res.send(`Abonné au canal ${channel}`);
 });
 
-app.get('/photos', async (req, res) => {
-  const cacheKey = 'photos';
-  const data = await client.get(cacheKey);
-  
-  if (data != null) {
-    // Données en cache disponibles
-    res.send(JSON.parse(data));
-  } else {
-    // Données non disponibles dans le cache, effectuez la requête
-    const users = await axios.get('https://jsonplaceholder.typicode.com/photos');
-    client.setEx(cacheKey, 3600, JSON.stringify(users.data)); // Mettez en cache pendant 1 heure
-    res.send(users.data);
-  }
-});
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+app.listen(3000, () => console.log('Server is running on port 3000'));
